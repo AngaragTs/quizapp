3c95e5995c887d086a0a01eef44367f789c50c93
25LP7649 25LP7649 <25LP7649@admins-iMac.local>
2026-02-04 09:59:54 +0800

    removed

--- src/app/api/generate/route.ts
+++
@@ -1,8 +0,0 @@
-import prisma from "@/lib/prisma";
-
-export const POST = async (request: Request) => {
-  const generate = await prisma.generate.create({
-    data: await request.json(),
-  });
-  return new Response(JSON.stringify({ generate }), { status: 201 });
-};
